68971e96b61f977f6d466be2bd3e50f4a866eee5
unknown <g00470444@atu.ie>
2026-02-23 16:35:34 +0000

    ghdfh

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Listcomponent } from './listcomponent/listcomponent';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Listcomponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Lab4');
-}
+  //register valuables
+  count :number = 0;
+  text :string = "Look at the star";
+  displayedText :string = "";
+  TextShown :boolean = false;
+
+    //counter
+  Click(){
+    this.count++;
+  }
+    //Show text function
+    ShowText(){
+      this.TextShown = !this.TextShown;
+    }
+  }
